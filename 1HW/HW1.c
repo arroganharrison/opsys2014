9ef65e47f65b5fc6e47370b6ab999d37e914889d
Harrison <harrison_tarr@yahoo.com>
2014-09-24 21:39:39 -0400

    just need to do redirect

--- 1HW/HW1.c
+++ 1HW/HW1.c
@@ -11,16 +11,8 @@
 #define HISTORY_SIZE 1000
 #define MAX_PATH_LENGTH 1000
 
-
-
-void executeCommand(char* command) {
-	#ifdef DEBUG
-		printf("Inside executeCommand\n");
-		printf("%s\n", command);
-	#endif
-		
+int getPathArray(char** pathArray) {
 	char* path = getenv("MYPATH");
-	char* pathArray[MAX_PATH_LENGTH];
 	pathArray[0] = strtok(path, ":");
 	int i = 0;
 	while (pathArray[i] != NULL) {
@@ -28,6 +20,140 @@ void executeCommand(char* command) {
 		pathArray[i] = strtok(NULL, ":");
 	}
 	int pathLength = i;
+	return pathLength;
+}
+
+void deleteConnec(int temp) {
+	pid_t pid = waitpid(-1, NULL, WNOHANG);
+	if (pid > 0) {
+		printf("[process %d completed]\n", pid);
+	}
+}
+
+
+void executeWithPipe(char** commandArgs) {
+	char* firstCommand[MAX_COMMAND_LENGTH];
+	char* secondCommand[MAX_COMMAND_LENGTH];
+
+	int i = 0;
+	while (0 != strcmp(commandArgs[i], "|")) {
+		firstCommand[i] = commandArgs[i];
+		i++;
+	}
+	i++;
+	int j = i;
+	while (commandArgs[i] != NULL) {
+		secondCommand[i-j] = commandArgs[i];
+		i++;
+	}
+	#ifdef DEBUG
+		int test = 0;
+		while (firstCommand[test] != NULL) {
+			printf("%s\n", firstCommand[test]);
+			test++;
+		}
+		test = 0;
+		while (secondCommand[test] != NULL) {
+			printf("%s\n", secondCommand[test]);
+			test++;
+		}
+	#endif
+
+	char* pathArray[MAX_PATH_LENGTH];
+	int pathLength = getPathArray(pathArray);
+	int background = 0;
+
+	for (i = 0; i < pathLength; i++) {
+		char commandDir[100];
+		struct stat lstatBuff;
+		sprintf(commandDir, "%s/%s", pathArray[i], commandArgs[0]);
+		if (lstat(pathArray[i], &lstatBuff) == 0) {
+
+			#ifdef DEBUG
+				printf("%s\n", pathArray[i]);
+			#endif
+
+			int childPID = fork();
+
+			if (childPID == 0) {
+
+				int p[2];
+				int rc = pipe(p);
+
+				if ( rc < 0 ) {
+				    perror( "pipe() failed" );
+				    return;
+				}
+
+				int childPID2 = fork();
+				if (childPID2 == 0) {
+					close(p[0]);
+					dup2(p[1], 1);
+					execvp(firstCommand[0], firstCommand);
+				}
+				else {
+					close(p[1]);
+					dup2(p[0], 0);
+					execvp(secondCommand[0], secondCommand);
+				}
+				
+			}
+			else if (background) {
+				signal(SIGCHLD, &deleteConnec);
+				waitpid(-1, NULL, WNOHANG);
+			}
+			else {
+				pid_t tempPID = waitpid(childPID, NULL, 0);
+				printf("Process %d terminated.\n", tempPID);
+			}
+			break;
+		}
+	}
+
+	
+
+
+}
+void executeCommand(char* command) {
+	#ifdef DEBUG
+		printf("Inside executeCommand\n");
+		printf("%s\n", command);
+	#endif
+
+	
+	
+	int i = 0;
+
+	
+
+	char* commandArgs[MAX_COMMAND_LENGTH];
+	commandArgs[0] = strtok(command, " ");
+	//int p = 0;
+	int background = 0;
+	
+	i = 0;
+
+	while (commandArgs[i] != NULL) {
+		#ifdef DEBUG
+			printf("%s %d\n", commandArgs[i], i);
+		#endif
+		i++;
+		commandArgs[i] = strtok(NULL, " ");
+	}
+	int commandArgsLen = i;
+	for (i = 0; i < commandArgsLen; i++) {
+		if (0 == strcmp(commandArgs[i], "&")) {
+			//printf("%s\n", commandArgs[i]);
+			background = 1;
+		}
+		if (0 == (strcmp(commandArgs[i], "|"))) {
+			executeWithPipe(commandArgs);
+			return;
+		}
+	}
+
+	char* pathArray[MAX_PATH_LENGTH];
+	int pathLength = getPathArray(pathArray);
 
 	#ifdef DEBUG
 		for (i = 0; i < pathLength; i++) {
@@ -35,24 +161,25 @@ void executeCommand(char* command) {
 		}
 	#endif
 
-	char* commandArgs[MAX_COMMAND_LENGTH];
-	commandArgs[0] = strtok(command, " ");
-	i = 0;
-	while (commandArgs[i] != NULL) {
-		#ifdef DEBUG
-			printf("%s\n", commandArgs[i]);
-		#endif
-		i++;
-		commandArgs[i] = strtok(NULL, " ");
-		// if (strcmp(commandArgs[i], "|") == 0) {
-		// 	//do other things
-		// }
-	}
-
 	#ifdef DEBUG
+		printf("%s", command);
 		printf("------\n");
 	#endif
 
+	if (0 == strcmp(commandArgs[0], "cd")) {
+		chdir(commandArgs[1]);
+		return;
+	}
+	if (0 == strcmp(commandArgs[0], "echo")) {
+		if ('$' == commandArgs[1][0]) {
+			char* enviroVar = getenv(++commandArgs[1]);
+			printf("%s\n", enviroVar);
+		}
+		else {
+			printf("%s\n", commandArgs[1]);
+		}
+		return;
+	}
 	for (i = 0; i < pathLength; i++) {
 		char commandDir[100];
 		struct stat lstatBuff;
@@ -68,13 +195,22 @@ void executeCommand(char* command) {
 			if (childPID == 0) {
 				execvp(commandArgs[0], commandArgs);
 			}
-			else {
-				pid_t tempPID = wait(NULL);
-				printf("Process %d terminated.", tempPID);
+			else if (background) {
+				signal(SIGCHLD, &deleteConnec);
+				printf("[process running in background with pid %d]\n", childPID);
+				waitpid(-1, NULL, WNOHANG);
 			}
-			break;
+			else {
+				
+				pid_t tempPID = waitpid(childPID, NULL, 0);
+				printf("Process %d terminated.\n", tempPID);
+			}
+			return;
 		}
 	}
+
+	printf("ERROR: command '%s' not found\n", commandArgs[0]);
+	
 	
 }
 void findCommand(char* currentCommand, char commandHistory[][MAX_COMMAND_LENGTH], int currentCommandIndex) {
@@ -84,13 +220,14 @@ void findCommand(char* currentCommand, char commandHistory[][MAX_COMMAND_LENGTH]
 		printf("%c\n", currentCommand[1]);
 	#endif
 
-	char* referenceCommand;
+	char tempCommand[MAX_COMMAND_LENGTH];
 	if (currentCommand[1] == '!') {
-		referenceCommand = commandHistory[(currentCommandIndex-1)%HISTORY_SIZE];
+		strcpy(tempCommand, commandHistory[(currentCommandIndex-1)%HISTORY_SIZE]);
 		#ifdef DEBUG
-			printf("%s", referenceCommand);
+			printf("%s", tempCommand);
 		#endif
-		executeCommand(referenceCommand); //printf("%s\n", referenceCommand);
+		strcpy(commandHistory[currentCommandIndex%HISTORY_SIZE], tempCommand);
+		executeCommand(tempCommand); //printf("%s\n", referenceCommand);
 	}
 	else if (isdigit(currentCommand[1]) && isdigit(currentCommand[2]) && isdigit(currentCommand[3])) {
 		char indexAsString[4];
@@ -98,7 +235,10 @@ void findCommand(char* currentCommand, char commandHistory[][MAX_COMMAND_LENGTH]
 		sprintf(indexAsString, "%c%c%c", currentCommand[1], currentCommand[2], currentCommand[3]);
 		//printf("%s\n", indexAsString);
 		int index = atoi(indexAsString);
-		executeCommand(commandHistory[index]);
+		index--;
+		strcpy(tempCommand, commandHistory[index]);
+		strcpy(commandHistory[currentCommandIndex%HISTORY_SIZE], tempCommand);
+		executeCommand(tempCommand);
 		//the arithmetic converts the individual 3 digits into a usable integer
 	} 
 	else {
@@ -109,36 +249,45 @@ void findCommand(char* currentCommand, char commandHistory[][MAX_COMMAND_LENGTH]
 		commandLen--;
 		int i;
 		currentCommand++;
-		for (i = currentCommandIndex-1; i%HISTORY_SIZE > 0; i--) {
+		for (i = currentCommandIndex; i%HISTORY_SIZE > 0; i--) {
 			#ifdef DEBUG
 				printf("%s %s\n", currentCommand, commandHistory[i]);
 			#endif
 			if (strncmp(currentCommand, commandHistory[i], commandLen) == 0) {
-				executeCommand(commandHistory[i]);
+				strcpy(commandHistory[currentCommandIndex%HISTORY_SIZE], commandHistory[i]);
+				strcpy(tempCommand, commandHistory[i]);
+				executeCommand(tempCommand);
 				break;
 			}
 		}
-		for (i = 999; i >= currentCommandIndex; i--) {
+		for (i = 999; i > currentCommandIndex; i--) {
 			if (strncmp(currentCommand, commandHistory[i], commandLen) == 0) {
-				executeCommand(commandHistory[i]);
+				strcpy(commandHistory[currentCommandIndex%HISTORY_SIZE], commandHistory[i]);
+				strcpy(tempCommand, commandHistory[i]);
+				executeCommand(tempCommand);
 				break;
 			}
 		}
-		printf("%c\n", currentCommand[1]);
+		//printf("%c\n", currentCommand[1]);
 	}
 
 }
 
 int main(int argc, char* argv[]) {
 
-	printf("Inside main; printf\n");
+
 	char commandHistory[HISTORY_SIZE][MAX_COMMAND_LENGTH];
-	int currentCommandIndex = 1;
+	int currentCommandIndex = 0;
 	while (1) {
+		char cwd[1024];
+		getcwd(cwd, 1024);
+		printf("%s$ ", cwd);
+		fflush(0);
+
 		char currentCommand[MAX_COMMAND_LENGTH];
 		int sizeRead = read(0, currentCommand, MAX_COMMAND_LENGTH-1);
 		currentCommand[sizeRead-1] = '\0';
-		printf("%s\n", (char*) currentCommand);
+		//printf("%s\n", (char*) currentCommand);
 
 		#ifdef DEBUG
 			printf("%d\n", sizeRead);
@@ -152,6 +301,7 @@ int main(int argc, char* argv[]) {
 			#endif
 
 			findCommand(currentCommand, commandHistory, currentCommandIndex);
+			currentCommandIndex++;
 		}
 		else if (0 == strcmp(currentCommand, "exit\0") || 0 == strcmp(currentCommand, "quit\0")) {
 			#ifdef DEBUG
@@ -159,15 +309,36 @@ int main(int argc, char* argv[]) {
 			#endif
 			
 			printf("BYE\n");
+			exit(0);
 			break;
+		}
+		else if (0 == strcmp(currentCommand, "history\0")) {
+			#ifdef DEBUG
+				printf("Printing history: \n");
+			#endif
+			strcpy(commandHistory[currentCommandIndex%HISTORY_SIZE], currentCommand);
+			currentCommandIndex++;
+			int i = 0;
+			if (currentCommandIndex < 999) {
+				
+				for (; i < currentCommandIndex; i++) {
+					printf("%03d %s\n", i+1, commandHistory[i]);
+				}
+			}
+			else {
+				for (i = 0; i < 999; i++) {
+					printf("%03d %s\n", currentCommandIndex-HISTORY_SIZE+i, commandHistory[(currentCommandIndex+i)%HISTORY_SIZE] );
+				}
+			}
+
 		}
 		else {
 			#ifdef DEBUG
 				printf("Inside else\n");
 			#endif
 			strcpy(commandHistory[currentCommandIndex%HISTORY_SIZE], currentCommand);
-			executeCommand(currentCommand);
-			currentCommandIndex++;			
+			currentCommandIndex++;	
+			executeCommand(currentCommand);		
 		}
 	}
 	return EXIT_SUCCESS;
---
+++ 1HW/sleeper.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+  int i = 0;
+  while ( i < 5 )
+  {
+    printf( "HEY!  Get back to work.\n" );
+    sleep( 5 );
+    i++;
+  }
+
+  return 0;
+}
